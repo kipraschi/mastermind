ce7a65d3b26c1e8b9d12e8e5145886112eccace9
Kipras Chi <kipraschi@gmail.com>
2026-04-06 23:19:25 +0300

    feat: add human codemaker class with input logic

---
+++ lib/human_codemaker.rb
@@ -0,0 +1,25 @@
+require_relative 'codemaker'
+require_relative 'input_validatation'
+
+class HumanCodemaker < Codemaker
+
+  include InputValidation
+
+  def initialize(board)
+    super
+    puts 'You are the Codemaker.'
+  end
+
+  private
+
+  def create_code
+    print_conditions
+    print 'Your secret code: '
+    get_valid_input
+  end
+  
+  def print_conditions
+    puts "Choose #{@pegs} unique numbers from 1 to #{@colors}."
+  end
+
+end
